00a3ef1e753fe647194de56c2ddd65757772e7b0
zensai3805 <iwa.pc.sw.mo@gmail.com>
2021-08-08 05:47:20 +0000

    fix workflow

--- deploy/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-DIR='/home/isucon/isucon10'
-echo '== system restart =='
-sudo systemctl restart mysql
-sudo systemctl restart nginx
-sudo systemctl restart isuumo.python.service
-echo '== complete =='

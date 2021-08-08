b7ee3e463f5b8213ffd0483dcb00b7bd1bb20a3e
zensai3805 <iwa.pc.sw.mo@gmail.com>
2021-08-08 08:29:00 +0000

    dix

--- deploy/restart.sh
+++ deploy/restart.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 DIR='/home/isucon/isucon10'
 echo '== system restart =='
+if [ -f /var/log/mysql/mysql-slow.log ]; then
+  sudo mv /var/log/mysql/mysql-slow.log /var/log/mysql/mysql-slow.log.$(date "+%Y%m%d_%H%M%S")
+fi
 sudo systemctl restart mysql
 sudo systemctl status mysql
 sudo systemctl restart nginx
